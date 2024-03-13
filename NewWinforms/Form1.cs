6be7f88a0d3345b903d86bf6041c3a4937beb17b
DanHagen <dan.hagen@centralia.edu>
2024-03-13 11:46:42 -0700

    added button data

--- NewWinforms/Form1.cs
+++ NewWinforms/Form1.cs
@@ -11,5 +11,10 @@ namespace NewWinforms
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "I've been clicked";
+        }
     }
 }
